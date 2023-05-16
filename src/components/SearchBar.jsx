b2f4fba851abb4d9b4c4bde7d5498ecd47736e28
NazaRosales <nazarenorosales_@hotmail.com>
2023-05-16 12:10:43 -0300

    souliconDeProblemas

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+export function SearchBar(props) {
+   return (
+      <div>
+         <input type = "search" />
+         <button onClick= {props.onSearch}>Agregar</button>
+      </div>
+   );
+}
+
+export default SearchBar;
